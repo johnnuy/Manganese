5e0fed7537ca9ec5da3b1a01f76a3208cf70739c
Jonny <jonny.trafford@gmail.com>
2024-12-01 11:18:14 -0500

    cleaned up

--- manganese-2024/src/main/java/org/johnnuy/manganese/aoc2024/SolutionDay1.java
+++ manganese-2024/src/main/java/org/johnnuy/manganese/aoc2024/SolutionDay1.java
@@ -14,16 +14,16 @@ import org.johnnuy.manganese.utils.LineHandler;
 public class SolutionDay1 {
 
 	public static void main(String[] args) throws IOException {
-		System.out.println("Sample: %d".formatted(difference(new ClassPathReader("day1/sample_1.txt"))));		
+		System.out.println("Sample: %d".formatted(difference(new ClassPathReader("day1/sample_1.txt"))));
 		System.out.println("Problem: %d".formatted(difference(new ClassPathReader("day1/input_1.txt"))));
-		
-		System.out.println("Sample 2: %d".formatted(similarity(new ClassPathReader("day1/sample_2.txt"))));		
+
+		System.out.println("Sample 2: %d".formatted(similarity(new ClassPathReader("day1/sample_2.txt"))));
 		System.out.println("Problem 2: %d".formatted(similarity(new ClassPathReader("day1/input_2.txt"))));
 	}
-	
-	
+
 	/**
 	 * Part 1 Difference
+	 * 
 	 * @param reader
 	 * @return
 	 * @throws IOException
@@ -31,30 +31,28 @@ public class SolutionDay1 {
 	public static int difference(Reader reader) throws IOException {
 		List<Integer> col1 = new ArrayList<>();
 		List<Integer> col2 = new ArrayList<>();
-		
-		
+
 		new LineHandler((line) -> {
 			String[] comps = line.replaceAll("\\s+", " ").split(" ");
 			col1.add(Integer.parseInt(comps[0]));
 			col2.add(Integer.parseInt(comps[1]));
-			return true;			
+			return true;
 		}).handle(reader);
-		
+
 		Collections.sort(col1);
 		Collections.sort(col2);
-		
+
 		int diff = 0;
-		for (int i=0; i<col1.size(); i++) {
+		for (int i = 0; i < col1.size(); i++) {
 			diff += Math.abs(col1.get(i) - col2.get(i));
 		}
-		
+
 		return diff;
-		
-		
 	}
-	
+
 	/**
 	 * Part 2 Similarity
+	 * 
 	 * @param reader
 	 * @return
 	 * @throws IOException
@@ -62,21 +60,19 @@ public class SolutionDay1 {
 	public static int similarity(Reader reader) throws IOException {
 		Map<Integer, Integer> col1 = new HashMap<>();
 		Map<Integer, Integer> col2 = new HashMap<>();
-		
-		
+
 		new LineHandler((line) -> {
-			String[] comps = line.replaceAll("\\s+", " ").split(" ");						
+			String[] comps = line.replaceAll("\\s+", " ").split(" ");
 			col1.compute(Integer.parseInt(comps[0]), (k, v) -> v == null ? 1 : v + 1);
 			col2.compute(Integer.parseInt(comps[1]), (k, v) -> v == null ? 1 : v + 1);
-			return true;			
+			return true;
 		}).handle(reader);
-		
+
 		int sim = 0;
 		for (Map.Entry<Integer, Integer> entry : col1.entrySet()) {
 			sim += entry.getKey() * entry.getValue() * col2.getOrDefault(entry.getKey(), 0);
 		}
-		
+
 		return sim;
 	}
-	
 }
