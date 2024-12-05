221419d99ddd61f580fba488f3ae82c50c43e4a1
Jonny <jonny.trafford@gmail.com>
2024-12-04 22:05:58 -0500

    using TimeUnit

--- manganese-utils/src/main/java/org/johnnuy/manganese/utils/Timer.java
+++ manganese-utils/src/main/java/org/johnnuy/manganese/utils/Timer.java
@@ -1,16 +1,19 @@
 package org.johnnuy.manganese.utils;
 
+import java.util.concurrent.TimeUnit;
 import java.util.function.Supplier;
 
 public class Timer<T> {
 
+	
+	
 	public T time(String label, Supplier<T> supplier) {
 		long t1 = System.nanoTime();
 		try {
 			return supplier.get();
 		}
 		finally {
-			System.out.println("%s took %dms".formatted(label, (System.nanoTime() - t1) / 1_000_000));
+			System.out.println("%s took %dms".formatted(label, TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - t1)));
 		}
 	}
 }
