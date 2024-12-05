4bc6568a5ccfce7e86abd065d2701fd43fedd84e
Jonny <jonny.trafford@gmail.com>
2024-12-04 20:21:56 -0500

    added solution for day 4

--- manganese-2024/src/main/java/org/johnnuy/manganese/aoc2024/SolutionDay3.java
+++ manganese-2024/src/main/java/org/johnnuy/manganese/aoc2024/SolutionDay3.java
@@ -11,7 +11,7 @@ import org.johnnuy.manganese.utils.ClassPathReader;
 import org.johnnuy.manganese.utils.LineHandler;
 
 public class SolutionDay3 {
-	
+
 	private static Pattern MUL_PATTERN = Pattern.compile("mul\\(([0-9]{1,3}),([0-9]{1,3})\\)");
 	private static Pattern MUL_WITH_INSTRUCTION_PATTERN = Pattern.compile("((mul\\(([0-9]{1,3}),([0-9]{1,3})\\))|(do\\(\\))|(don't\\(\\)))");
 
@@ -22,14 +22,14 @@ public class SolutionDay3 {
 		System.out.println("Sample 2: %d".formatted(scanWithInstructions(new ClassPathReader("day3/sample_2.txt"))));
 		System.out.println("Problem 2: %d".formatted(scanWithInstructions(new ClassPathReader("day3/input_2.txt"))));
 	}
-	
+
 	public static int scan(Reader reader) throws IOException {
 		AtomicInteger sum = new AtomicInteger();
-		
+
 		new LineHandler((line, index) -> {
 			Matcher m = MUL_PATTERN.matcher(line);
 			int pos = 0;
-			while (m.find(pos)) {				
+			while (m.find(pos)) {
 				sum.addAndGet(Integer.valueOf(m.group(1)) * Integer.valueOf(m.group(2)));
 				pos = m.end();
 			}
@@ -38,24 +38,22 @@ public class SolutionDay3 {
 
 		return sum.get();
 	}
-	
+
 	public static int scanWithInstructions(Reader reader) throws IOException {
 		AtomicInteger sum = new AtomicInteger();
 		AtomicBoolean enabled = new AtomicBoolean(true);
-		
+
 		new LineHandler((line, index) -> {
 			Matcher m = MUL_WITH_INSTRUCTION_PATTERN.matcher(line);
 			int pos = 0;
 			while (m.find(pos)) {
 				if (m.group(5) != null) {
 					enabled.set(true);
-				}
-				else if (m.group(6) != null) {
+				} else if (m.group(6) != null) {
 					enabled.set(false);
-				}
-				else if (enabled.get()) {
+				} else if (enabled.get()) {
 					sum.addAndGet(Integer.valueOf(m.group(3)) * Integer.valueOf(m.group(4)));
-				}				
+				}
 				pos = m.end();
 			}
 			return true;
---
+++ manganese-2024/src/main/java/org/johnnuy/manganese/aoc2024/SolutionDay4.java
@@ -0,0 +1,159 @@
+package org.johnnuy.manganese.aoc2024;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.johnnuy.manganese.utils.ClassPathReader;
+import org.johnnuy.manganese.utils.LineHandler;
+
+public class SolutionDay4 {
+
+	private static int[][] LINEAR_SEARCH_VECTORS = new int[][] {
+			{ -1, 0 },
+			{ -1, 1 },
+			{ 0, 1 },
+			{ 1, 1 },
+			{ 1, -1 },
+			{ -1, -1 },
+			{ 1, 0 },
+			{ 0, -1 }
+	};
+
+	private static int[][] CROSS_SEARCH_VECTORS = new int[][] {
+			{ 1, 1 },
+			{ -1, -1 }
+	};
+
+	public static void main(String[] args) throws IOException {
+		System.out.println("Sample: %d".formatted(searchXMAS(new ClassPathReader("day4/sample_1.txt"))));
+		System.out.println("Problem: %d".formatted(searchXMAS(new ClassPathReader("day4/input_1.txt"))));
+
+		System.out.println("Sample 2: %d".formatted(searchX_MAS(new ClassPathReader("day4/sample_2.txt"))));
+		System.out.println("Problem 2: %d".formatted(searchX_MAS(new ClassPathReader("day4/input_2.txt"))));
+	}
+
+	public static int searchXMAS(Reader reader) throws IOException {
+		AtomicInteger count = new AtomicInteger();
+		List<String> tmp = new ArrayList<>();
+
+		new LineHandler((line, index) -> {
+			tmp.add(line);
+			return true;
+		}).handle(reader);
+
+		char[][] grid = gridify(tmp);
+		int rows = grid.length;
+		int cols = grid[0].length;
+
+		for (int x = 0; x < rows; x++) {
+			for (int y = 0; y < cols; y++) {
+				/* only continue if we start on an 'X' */
+				if (extract(grid, x, y) != 'X') {
+					continue;
+				}
+				/* search all our vectors */
+				for (int v = 0; v < LINEAR_SEARCH_VECTORS.length; v++) {
+					if (searchLine(grid, x, y, LINEAR_SEARCH_VECTORS[v][0], LINEAR_SEARCH_VECTORS[v][1])) {
+						count.incrementAndGet();
+					}
+				}
+			}
+		}
+
+		return count.get();
+	}
+
+	public static int searchX_MAS(Reader reader) throws IOException {
+		AtomicInteger count = new AtomicInteger();
+		List<String> tmp = new ArrayList<>();
+
+		new LineHandler((line, index) -> {
+			tmp.add(line);
+			return true;
+		}).handle(reader);
+
+		char[][] grid = gridify(tmp);
+		int rows = grid.length;
+		int cols = grid[0].length;
+
+		for (int x = 0; x < rows; x++) {
+			for (int y = 0; y < cols; y++) {
+				/* only continue if we start on an 'A' */
+				if (extract(grid, x, y) != 'A') {
+					continue;
+				}
+				/* search all our vectors */
+				vectors: for (int v = 0; v < CROSS_SEARCH_VECTORS.length; v++) {
+					if (searchX(grid, x, y, CROSS_SEARCH_VECTORS[v][0], CROSS_SEARCH_VECTORS[v][1])) {
+						count.incrementAndGet();
+						break vectors;
+					}
+				}
+			}
+		}
+
+		return count.get();
+	}
+
+	private static boolean searchX(char[][] grid, int x, int y, int dx, int dy) {
+		return extract(grid, x + dx, y + dy) == 'M' && extract(grid, x - dx, y - dy) == 'S' &&
+				((extract(grid, x + dx, y - dy) == 'M' && extract(grid, x - dx, y + dy) == 'S') ||
+						(extract(grid, x + dx, y - dy) == 'S' && extract(grid, x - dx, y + dy) == 'M'));
+	}
+
+	/**
+	 * Extract 3 characters along our search vector and ensure we get the correct
+	 * value
+	 * 
+	 * @param grid
+	 * @param x
+	 * @param y
+	 * @param dx
+	 * @param dy
+	 * @return
+	 */
+	private static boolean searchLine(char[][] grid, int x, int y, int dx, int dy) {
+		return extract(grid, x + (dx * 1), y + (dy * 1)) == 'M' &&
+				extract(grid, x + (dx * 2), y + (dy * 2)) == 'A' &&
+				extract(grid, x + (dx * 3), y + (dy * 3)) == 'S';
+	}
+
+	/**
+	 * Extract the character from the grid, return null character if outside the
+	 * grid
+	 * 
+	 * @param grid
+	 * @param x
+	 * @param y
+	 * @return
+	 */
+	private static char extract(char[][] grid, int x, int y) {
+		try {
+			char v = grid[x][y];
+			return v;
+		} catch (IndexOutOfBoundsException e) {
+			return '\0';
+		}
+	}
+
+	/**
+	 * Generate a grid of chars
+	 * 
+	 * @param data
+	 * @return
+	 */
+	private static char[][] gridify(List<String> data) {
+		char[][] grid = new char[data.size()][];
+		for (int i = 0; i < data.size(); i++) {
+			String row = data.get(i);
+			grid[i] = new char[row.length()];
+			for (int j = 0; j < row.length(); j++) {
+				grid[i][j] = row.charAt(j);
+			}
+		}
+		return grid;
+	}
+}
