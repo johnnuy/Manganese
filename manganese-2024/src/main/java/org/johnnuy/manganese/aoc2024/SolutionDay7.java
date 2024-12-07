68dc53b32d9ca82415e25bf4ab518b622b80e675
Jonny <jonny.trafford@gmail.com>
2024-12-07 12:09:08 -0500

    added Part 2 day 7

--- manganese-2024/src/main/java/org/johnnuy/manganese/aoc2024/SolutionDay7.java
+++ manganese-2024/src/main/java/org/johnnuy/manganese/aoc2024/SolutionDay7.java
@@ -6,6 +6,7 @@ import java.math.BigInteger;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.BiFunction;
 
 import org.johnnuy.manganese.utils.ClassPathReader;
 import org.johnnuy.manganese.utils.LineHandler;
@@ -14,34 +15,42 @@ import org.johnnuy.manganese.utils.Timer;
 public class SolutionDay7 {
 
 	private static Timer<BigInteger> timer = new Timer<>();
+	private static final List<BiFunction<Long, Long, Long>> PART1_OPERATORS = List.of(
+			(x, y) -> x + y,
+			(x, y) -> x * y);
+	
+	private static final List<BiFunction<Long, Long, Long>> PART2_OPERATORS = List.of(
+			(x, y) -> x + y,
+			(x, y) -> x * y,
+			(x, y) -> Long.parseLong("%d%d".formatted(x, y)));
 
 	public static void main(String[] args) throws IOException {
-		System.out.println("Sample 1: %d\n".formatted(timer.time("Sample 1", () -> correct(new ClassPathReader("day7/sample_1.txt")))));
-		System.out.println("Problem 1: %d\n".formatted(timer.time("Problem 1", () -> correct(new ClassPathReader("day7/input_1.txt")))));
-
-//		System.out.println("Sample 2: %d\n".formatted(timer.time("Sample 2", () -> patrolWithLoopCounter(new ClassPathReader("day7/sample_2.txt")))));
-//		System.out.println("Problem 2: %d\n".formatted(timer.time("Problem 2", () -> patrolWithLoopCounter(new ClassPathReader("day7/input_2.txt")))));
+		System.out.println("Sample 1: %d\n".formatted(timer.time("Sample 1", () -> correct(new ClassPathReader("day7/sample_1.txt"), PART1_OPERATORS))));
+		System.out.println("Problem 1: %d\n".formatted(timer.time("Problem 1", () -> correct(new ClassPathReader("day7/input_1.txt"), PART1_OPERATORS))));
+		
+		System.out.println("Sample 2: %d\n".formatted(timer.time("Sample 2", () -> correct(new ClassPathReader("day7/sample_2.txt"), PART2_OPERATORS))));
+		System.out.println("Problem 2: %d\n".formatted(timer.time("Problem 2", () -> correct(new ClassPathReader("day7/input_2.txt"), PART2_OPERATORS))));
 	}
 	
-	public static BigInteger correct(Reader reader) {
+	public static BigInteger correct(Reader reader, List<BiFunction<Long, Long, Long>> operations) {
 		AtomicReference<BigInteger> counter = new AtomicReference<>(BigInteger.ZERO);
 		
 		new LineHandler((line, index) -> {			
-			counter.set(counter.get().add(BigInteger.valueOf(evaluate(line))));
+			counter.set(counter.get().add(BigInteger.valueOf(evaluate(line, operations))));
 			return true;
 		}).handle(reader);
 		
 		return counter.get();
 	}
 	
-	private static long evaluate(String line) {
+	private static long evaluate(String line, List<BiFunction<Long, Long, Long>> operations) {
 		int index = line.indexOf(':');
 		long value = Long.parseLong(line.substring(0, index));		
 		List<Long> operands = Arrays
 				.stream(line.substring(index + 2).split("\\s+"))
 				.map(Long::parseLong)
 				.toList();
-		if (evaluate(value, operands)) {
+		if (evaluate(operands.get(0), value, operands.subList(1, operands.size()), operations)) {
 			return value;
 		}
 		else {
@@ -49,22 +58,21 @@ public class SolutionDay7 {
 		}
 	}
 	
-	private static boolean evaluate(long value, List<Long> operands) {
-		if (operands.size() == 1) {			
-			return operands.get(0).equals(value);
-		}		
+	private static boolean evaluate(long value, long target, List<Long> operands, List<BiFunction<Long, Long, Long>> operations) {
+		if (operands.size() == 0) {
+			return value == target;
+		}
+		if (value > target) {
+			return false;
+		}
 		
-		/* subtract the last value and search recursively with the prefixed sublist */
-		long finalOperand = operands.get(operands.size() - 1);
-		if (evaluate(value - finalOperand, operands.subList(0, operands.size() - 1))) {
-			return true;
-		}
-		/* if we have a modulus 0, this could work */		
-		if ((value % finalOperand) == 0 && evaluate(value / finalOperand, operands.subList(0, operands.size() - 1))) {
-			return true;
-		}
-				
+		/* subtract the last value and search recursively with the prefixed sublist */		
+		long firstOperand = operands.get(0);
+		for(BiFunction<Long, Long, Long> operation : operations) {
+			if (evaluate(operation.apply(value, firstOperand), target, operands.subList(1, operands.size()), operations)) {
+				return true;
+			}
+		};		
 		return false;
-		
 	}
 }
