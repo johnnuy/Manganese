d27fbc277697946429be72d834b5a7ec48009f8b
Jonny <jonny.trafford@gmail.com>
2024-12-07 13:53:52 -0500

    added inverse correction which is 50 times faster

--- manganese-2024/src/main/java/org/johnnuy/manganese/aoc2024/SolutionDay7.java
+++ manganese-2024/src/main/java/org/johnnuy/manganese/aoc2024/SolutionDay7.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.BiFunction;
 
+import org.apache.commons.lang3.tuple.Pair;
 import org.johnnuy.manganese.utils.ClassPathReader;
 import org.johnnuy.manganese.utils.LineHandler;
 import org.johnnuy.manganese.utils.Timer;
@@ -23,13 +24,31 @@ public class SolutionDay7 {
 			(x, y) -> x + y,
 			(x, y) -> x * y,
 			(x, y) -> Long.parseLong("%d%d".formatted(x, y)));
+	
+	private static final List<Pair<BiFunction<Long, Long, Boolean>, BiFunction<Long, Long, Long>>> PART1_OPERATIONS_INVERSE = List.of(
+			Pair.of((x, y) -> x > y, (x, y) -> x - y),
+			Pair.of((x, y) -> x % y == 0, (x, y) -> x / y)
+			);
+	
+	private static final List<Pair<BiFunction<Long, Long, Boolean>, BiFunction<Long, Long, Long>>> PART2_OPERATIONS_INVERSE = List.of(
+			Pair.of((x, y) -> x > y, (x, y) -> x - y),
+			Pair.of((x, y) -> x % y == 0, (x, y) -> x / y),
+			Pair.of(SolutionDay7::endsWith, SolutionDay7::unconcatenate)
+			);
+			
 
 	public static void main(String[] args) throws IOException {
 		System.out.println("Sample 1: %d\n".formatted(timer.time("Sample 1", () -> correct(new ClassPathReader("day7/sample_1.txt"), PART1_OPERATORS))));
 		System.out.println("Problem 1: %d\n".formatted(timer.time("Problem 1", () -> correct(new ClassPathReader("day7/input_1.txt"), PART1_OPERATORS))));
 		
+		System.out.println("Sample 1: %d\n".formatted(timer.time("Sample 1", () -> correctReverse(new ClassPathReader("day7/sample_1.txt"), PART1_OPERATIONS_INVERSE))));
+		System.out.println("Problem 1: %d\n".formatted(timer.time("Problem 1", () -> correctReverse(new ClassPathReader("day7/input_1.txt"), PART1_OPERATIONS_INVERSE))));
+		
 		System.out.println("Sample 2: %d\n".formatted(timer.time("Sample 2", () -> correct(new ClassPathReader("day7/sample_2.txt"), PART2_OPERATORS))));
 		System.out.println("Problem 2: %d\n".formatted(timer.time("Problem 2", () -> correct(new ClassPathReader("day7/input_2.txt"), PART2_OPERATORS))));
+		
+		System.out.println("Sample 2: %d\n".formatted(timer.time("Sample 2", () -> correctReverse(new ClassPathReader("day7/sample_2.txt"), PART2_OPERATIONS_INVERSE))));
+		System.out.println("Problem 2: %d\n".formatted(timer.time("Problem 2", () -> correctReverse(new ClassPathReader("day7/input_2.txt"), PART2_OPERATIONS_INVERSE))));
 	}
 	
 	public static BigInteger correct(Reader reader, List<BiFunction<Long, Long, Long>> operations) {
@@ -74,5 +93,77 @@ public class SolutionDay7 {
 			}
 		};		
 		return false;
+	}
+	
+	
+	public static BigInteger correctReverse(Reader reader, List<Pair<BiFunction<Long, Long, Boolean>, BiFunction<Long, Long, Long>>> operations) {
+		AtomicReference<BigInteger> counter = new AtomicReference<>(BigInteger.ZERO);
+		
+		new LineHandler((line, index) -> {			
+			counter.set(counter.get().add(BigInteger.valueOf(evaluateReverse(line, operations))));
+			return true;
+		}).handle(reader);
+		
+		return counter.get();
+	}
+	
+	private static long evaluateReverse(String line, List<Pair<BiFunction<Long, Long, Boolean>, BiFunction<Long, Long, Long>>> operations) {
+		int index = line.indexOf(':');
+		long value = Long.parseLong(line.substring(0, index));		
+		List<Long> operands = Arrays
+				.stream(line.substring(index + 2).split("\\s+"))
+				.map(Long::parseLong)
+				.toList();
+		if (evaluateReverse(value, operands, operations)) {
+			return value;
+		}
+		else {
+			return 0;
+		}
+	}
+	
+	private static boolean evaluateReverse(long value, List<Long> operands, List<Pair<BiFunction<Long, Long, Boolean>, BiFunction<Long, Long, Long>>> operations) {
+		if (operands.size() == 1) {			
+			return operands.get(0).equals(value);
+		}		
+		
+		long finalOperand = operands.get(operands.size() - 1);
+		for(Pair<BiFunction<Long, Long, Boolean>, BiFunction<Long, Long, Long>> operation: operations) {
+			if (operation.getLeft().apply(value, finalOperand) && evaluateReverse(operation.getRight().apply(value, finalOperand), operands.subList(0, operands.size() - 1), operations)) {
+				return true;
+			}
+		};		
+				
+		return false;
+	}
+	
+	private static boolean endsWith(long number, long suffix) {
+		int numDigits = digits(suffix);   
+
+        long divisor = (long) Math.pow(10, numDigits);
+        
+        /* run the modulus with our divisor to compare to suffix */
+        return number % divisor == suffix;
+    }
+	
+	private static long unconcatenate(long number, long suffix) {
+		int numDigits = digits(suffix);        
+        
+        long divisor = (long) Math.pow(10, numDigits);
+        
+        return number / divisor;
+	}
+	
+	private static int digits(long x) {
+		if (x == 0) {
+			return 1;
+		}
+		long temp = x;
+        int numDigits = 0;
+        while (temp > 0) {
+            numDigits++;
+            temp /= 10;
+        }
+        return numDigits;
 	}
 }
