7adb3c30b8adb81950551ad7a3b2e85d91b33e78
Jonny <jonny.trafford@gmail.com>
2024-12-09 23:54:49 -0500

    cleaned up day 9

--- manganese-2024/src/main/java/org/johnnuy/manganese/aoc2024/SolutionDay9.java
+++ manganese-2024/src/main/java/org/johnnuy/manganese/aoc2024/SolutionDay9.java
@@ -10,60 +10,59 @@ import java.util.function.Consumer;
 import org.johnnuy.manganese.utils.ClassPathReader;
 import org.johnnuy.manganese.utils.LineHandler;
 import org.johnnuy.manganese.utils.Timer;
+import org.springframework.util.Assert;
 
 public class SolutionDay9 {
 
 	private static Timer<Long> timer = new Timer<>();
-	
 
 	public static void main(String[] args) throws IOException {
 		System.out.println("Sample 1: %d\n".formatted(timer.time("Sample 1", () -> process(new ClassPathReader("day9/sample_1.txt"), SolutionDay9::compactIndividual))));
 		System.out.println("Problem 1: %d\n".formatted(timer.time("Problem 1", () -> process(new ClassPathReader("day9/input_1.txt"), SolutionDay9::compactIndividual))));
-		
+
 		System.out.println("Sample 2: %d\n".formatted(timer.time("Sample 2", () -> process(new ClassPathReader("day9/sample_2.txt"), SolutionDay9::compactComplete))));
-		System.out.println("Problem 2: %d\n".formatted(timer.time("Problem 2", () -> process(new ClassPathReader("day9/input_1.txt"), SolutionDay9::compactComplete))));
+		System.out.println("Problem 2: %d\n".formatted(timer.time("Problem 2", () -> process(new ClassPathReader("day9/input_2.txt"), SolutionDay9::compactComplete))));
 	}
-	
+
 	public static Long process(Reader reader, Consumer<List<Integer>> compaction) {
-		
+
 		AtomicLong value = new AtomicLong();
-		
+
 		new LineHandler((line, index) -> {
-			
+
 			List<Integer> expansion = expand(line);
-			
+
 			compaction.accept(expansion);
-			
+
 			value.set(calculate(expansion));
-			
+
 			return true;
 		}).handle(reader);
-			
+
 		return value.get();
 	}
 
 	private static List<Integer> expand(String line) {
 		List<Integer> expansion = new ArrayList<>();
 		int fileId = -1;
-		for (int i=0; i<line.length(); i++) {
-			if (i%2 == 0) {
+		for (int i = 0; i < line.length(); i++) {
+			if (i % 2 == 0) {
 				fileId++;
-				for (int j=0; j<Integer.valueOf("%s".formatted(line.charAt(i))); j++) {
+				for (int j = 0; j < Integer.valueOf("%s".formatted(line.charAt(i))); j++) {
 					expansion.add(fileId);
 				}
-			}
-			else {
-				for (int j=0; j<Integer.valueOf("%s".formatted(line.charAt(i))); j++) {
+			} else {
+				for (int j = 0; j < Integer.valueOf("%s".formatted(line.charAt(i))); j++) {
 					expansion.add(-1);
 				}
 			}
 		}
 		return expansion;
 	}
-	
+
 	private static void compactIndividual(List<Integer> list) {
 		int tail = list.size() - 1;
-		for (int head=0; head<tail; head++) {
+		for (int head = 0; head < tail; head++) {
 			if (list.get(head) == -1) {
 				while (list.get(tail) == -1) {
 					tail--;
@@ -77,54 +76,48 @@ public class SolutionDay9 {
 			}
 		}
 	}
-	
+
 	private static void compactComplete(List<Integer> list) {
-		System.out.println(list);		
 		/* initialize tail to the end of the list */
 		int tail = list.size() - 1;
 		int lastFileId = list.get(tail);
-		
+
 		/* attempt to move files from the end forward */
-		for (int f=lastFileId; f>=0; f--) {
+		for (int f = lastFileId; f >= 0; f--) {
 			int fileSize = 0;
-			while (tail >=0 && list.get(tail) == f) {
+			while (tail >= 0 && list.get(tail) == f) {
 				fileSize++;
 				tail--;
-			}			
-			
-//			System.out.println("Current File Id %d had size %d".formatted(f, fileSize));
-			
+			}
+
 			/* start from our head, and search for a continuous open section */
 			int count = 0;
-//			System.out.println("Trying to move %d with size %d".formatted(f, fileSize));
-			for (int i=0; i<tail; i++) {
+			for (int i = 0; i <= tail; i++) {
 				if (list.get(i) == -1) {
 					count++;
 					/* found a contiguous block, so move the data */
 					if (count == fileSize) {
-//						System.out.println("Moving %d".formatted(f));
-						for (int j=0; j<fileSize; j++) {
+						for (int j = 0; j < fileSize; j++) {
+							Assert.isTrue(list.get(i + 1 - fileSize + j) == -1, "element at %d was not -1".formatted(i + 1 - fileSize + j));
 							list.set(i + 1 - fileSize + j, list.get(tail + 1 + j));
 							list.set(tail + 1 + j, -1);
 						}
 						break;
-					}					
-				}
-				else {
+					}
+				} else {
 					count = 0;
 				}
 			}
-			
-			while (tail >= 0 && (list.get(tail) != (f-1))) {
-				tail --;
+
+			while (tail >= 0 && (list.get(tail) != (f - 1))) {
+				tail--;
 			}
 		}
-		System.out.println(list);
 	}
-	
+
 	private static long calculate(List<Integer> list) {
 		long value = 0;
-		for (int pos=0; pos<list.size(); pos++) {			
+		for (int pos = 0; pos < list.size(); pos++) {
 			if (list.get(pos) != -1) {
 				value += (pos * list.get(pos));
 			}
