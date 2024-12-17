75d5a296bfe2307085b174e2665ed5aaf27e6e5e
Jonny <jonny.trafford@gmail.com>
2024-12-16 19:17:03 -0500

    it's slow... but it works.

--- manganese-2024/src/main/java/org/johnnuy/manganese/aoc2024/SolutionDay16.java
+++ manganese-2024/src/main/java/org/johnnuy/manganese/aoc2024/SolutionDay16.java
@@ -29,9 +29,6 @@ public class SolutionDay16 {
 		System.out.println("Sample 1a: %d\n".formatted(timer.time("Sample 1a", () -> eval(new ClassPathReader("day16/sample_1a.txt")))));
 		System.out.println("Sample 1b: %d\n".formatted(timer.time("Sample 1b", () -> eval(new ClassPathReader("day16/sample_1b.txt")))));
 		System.out.println("Problem 1: %d\n".formatted(timer.time("Problem 1", () -> eval(new ClassPathReader("day16/input_1.txt")))));
-
-//		System.out.println("Sample 2: %d\n".formatted(timer.time("Sample 2", () -> eval2(new ClassPathReader("day15/sample_2.txt")))));
-//		System.out.println("Problem 2: %d\n".formatted(timer.time("Problem 2", () -> eval2(new ClassPathReader("day15/input_2.txt")))));
 	}
 
 	public static Integer eval(Reader reader) {
@@ -59,66 +56,64 @@ public class SolutionDay16 {
 		GridUtils.printGrid(cGrid);
 		cGrid[reindeer.position().y()][reindeer.position().x()] = '.';		
 		Set<Position> bestPositions = new HashSet<>();
-		move(cGrid, start.get(), CardinalDirection.EAST, 0, shortest, false, new HashMap<>(), new Stack<>(), bestPositions);
+		move(cGrid, start.get(), CardinalDirection.EAST, 0, shortest, false, new HashMap<>(), new Stack<>(), bestPositions, 0);
 		System.out.println("Found %d best positions".formatted(bestPositions.size() + 1));
 		
-//		System.out.println(bestPositions);		
-//		for(Position p : bestPositions) {
-//			cGrid[p.y()][p.x()] = 'O';
-//		}
-//		GridUtils.printGrid(cGrid);
+		System.out.println(bestPositions);		
+		for(Position p : bestPositions) {
+			cGrid[p.y()][p.x()] = 'O';
+		}
+		GridUtils.printGrid(cGrid);
 
 		return shortest.get();
 	}
 	
-	private static void move(char[][] cGrid, Position p, CardinalDirection dir, int path, AtomicInteger shortest, boolean turned, Map<Position, Integer> shortestMap, Stack<Position> positions, Set<Position> bestPositions) {
+	private static void move(char[][] cGrid, Position p, CardinalDirection dir, int path, AtomicInteger shortest, boolean turned, Map<Position, Integer> shortestMap, List<Position> positions, Set<Position> bestPositions, int turns) {
 		try {
-//			/* push our current position onto the stack */
-//			if (!turned) {
-//				positions.push(p);
-//			}
-			
-			/* check if we've been to this position already */
-			if (!shortestMap.containsKey(p)) {
-				shortestMap.put(p, path);			
-			}
-			else {			
-				Integer value = shortestMap.get(p);
-				/* don't continue if we already have a shorter path to this spot */
-				if (path <= value) {
-					/* update our shortest map */
-					shortestMap.put(p, path);
-				}
-				else if (!turned) { 
-//					System.out.println("Currently on longer path to %s with path length %d, previous length %d".formatted(p, path, value));
-					return;
-				}
+			/* push our current position onto the stack */
+			if (!turned) {
+				positions.add(p);
 			}
 			
 			char c = GridUtils.extract(cGrid, p.x() + dir.getDx(), p.y() + dir.getDy());
 			
 			/* we can move forwards */
-			if (c == '.') {				
-				/* mark this position as taken to avoid cycles */
-				cGrid[p.y()][p.x()] = 'X';
-				Position next = p.move(new Direction(dir.getDx(), dir.getDy()));
-				
-//				cGrid[next.y()][next.x()] = 'S';
-//				GridUtils.printGrid(cGrid);
-//				cGrid[next.y()][next.x()] = '.';
-				
-				move(cGrid, next, dir, path + 1, shortest, false, shortestMap, positions, bestPositions);
-				
-				/* reset our grid, this isn't a spot we want */
-				cGrid[p.y()][p.x()] = '.';
+			if (c == '.') {		
+				try {
+					/* mark this position as taken to avoid cycles */
+					cGrid[p.y()][p.x()] = 'X';
+					Position next = p.move(new Direction(dir.getDx(), dir.getDy()));
+					
+					/* check if we've been to this position already */
+					if (!shortestMap.containsKey(next)) {
+						shortestMap.put(next, turns);			
+					}
+					else {			
+						Integer value = shortestMap.get(next);
+						/* don't continue if we already have a shorter path to this spot */
+						if (turns <= value) {
+							/* update our shortest map */
+							shortestMap.put(next, turns);
+						}
+						else if (turns > value + 1) {
+							return;
+						}
+					}
+					
+					move(cGrid, next, dir, path + 1, shortest, false, shortestMap, positions, bestPositions, turns);
+				}
+				finally {				
+					/* reset our grid, this isn't a spot we want */
+					cGrid[p.y()][p.x()] = '.';
+				}
 			}
 			else if (c == 'E') {
 				if (shortest.get() >= (path + 1)) {
 					System.out.println("Found shortest path yet as %d with tiles %d".formatted(path + 1, positions.size()));
-//					if (shortest.get() > (path + 1)) {
-//						bestPositions.clear();
-//					}
-//					bestPositions.addAll(positions);
+					if (shortest.get() > (path + 1)) {
+						bestPositions.clear();
+					}
+					bestPositions.addAll(positions);
 					shortest.set(path + 1);
 				}
 				return;
@@ -126,19 +121,19 @@ public class SolutionDay16 {
 			
 			if (!turned) {
 				/* try turning clockwise */
-				move(cGrid, p, CardinalDirection.clockwise(dir), path + 1000, shortest, true, shortestMap, positions, bestPositions);
+				move(cGrid, p, CardinalDirection.clockwise(dir), path + 1000, shortest, true, shortestMap, positions, bestPositions, turns + 1);
 				
 				/* try turning counterclockwise */
-				move(cGrid, p, CardinalDirection.counterClockwise(dir), path + 1000, shortest, true, shortestMap, positions, bestPositions);
+				move(cGrid, p, CardinalDirection.counterClockwise(dir), path + 1000, shortest, true, shortestMap, positions, bestPositions, turns + 1);
 							
 				/* try turning backwards (could happen at the start) */
-				move(cGrid, p, CardinalDirection.clockwise(CardinalDirection.clockwise(dir)), path + 2000, shortest, true, shortestMap, positions, bestPositions);			
+				move(cGrid, p, CardinalDirection.clockwise(CardinalDirection.clockwise(dir)), path + 2000, shortest, true, shortestMap, positions, bestPositions, turns + 1);			
 			}
 		}
 		finally {
-//			if (!turned) {
-//				positions.pop();
-//			}
+			if (!turned) {
+				positions.remove(positions.size() - 1);
+			}
 		}
 	}
 }
